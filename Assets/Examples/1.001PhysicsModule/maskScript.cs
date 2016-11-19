eb33b107cdf60f568b9869a6a9e165f9501b0e32
Timwi <github@timwi.de>
2016-11-19 13:42:55 +0100

    Konqi’s original code.

---
+++ Assets/Examples/1.001PhysicsModule/maskScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class maskScript : MonoBehaviour {
+
+	Light mazeLigth;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake(){
+		mazeLigth = GetComponent<Light> ();
+		mazeLigth.cullingMask = 1 << 30;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
